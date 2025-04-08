2cc63be6e764ecada00bbe0352ba65e9e864ae04
Anastasia Planidina <aplanidina93@gmail.com>
2025-04-08 11:42:15 +0400

    добавила проверки после сабмита формы и поменяла название теста

--- test_login.py
+++ test_login.py
@@ -2,7 +2,7 @@ from selene import browser, have, be
 from conftest import baseurl
 
 
-def test_fill_name_and_lastname():
+def test_fill_the_form():
     browser.open(baseurl)
     browser.element('#firstName').click().type('Anastasia')
     browser.element('#lastName').click().type('Anastasia')
@@ -10,7 +10,9 @@ def test_fill_name_and_lastname():
     browser.element('[for="gender-radio-2"]').click().should(be.existing)
     browser.element('[placeholder="Mobile Number"]').click().type('8888888889')
     browser.element('#dateOfBirthInput').click()
-    browser.element('#dateOfBirthInput').click().clear().type('11.11.1999').click()
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').double_click()
+    browser.element('.react-datepicker__year-select').double_click()
     browser.element('[for="hobbies-checkbox-1"]').click()
     browser.element('#uploadPicture').should(be.visible)
     browser.driver.execute_script("window. scrollBy(0, 800)")
@@ -20,3 +22,10 @@ def test_fill_name_and_lastname():
     assert submit.should(be.existing)
     browser.element('#submit').click()
     browser.element('.modal-title').should(have.text('Thanks for submitting the form'))
+    browser.element('tbody').should(be.visible)
+    browser.element('tbody').should(have.text('Anastasia Anastasia'))
+    browser.element('tbody').should(have.text('anyemail@any.mail'))
+    browser.element('tbody').should(have.text('Female'))
+    browser.element('tbody').should(have.text('8888888889'))
+    browser.element('tbody').should(have.text('08 April,2025'))
+    browser.element('tbody').should(have.text('Sports'))
